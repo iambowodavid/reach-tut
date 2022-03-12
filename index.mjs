d98df080ec8afe4bd4d7d6788ba8814531e0ad0d
Bowo David <davidbowo@gmail.com>
2022-03-12 14:26:46 +0100

    add .vscode and .reach to gitignore

--- index.mjs
+++ index.mjs
@@ -9,16 +9,46 @@ const startingBalance = stdlib.parseCurrency(100);
 const accAlice = await stdlib.newTestAccount(startingBalance);
 const accBob = await stdlib.newTestAccount(startingBalance);
 
+//record starting balances of each participant
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
+
 //deploy the ontract application
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+//setup hands and the possible outcomes
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const Player = (Who) => ({
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
+
 //now let's implement Alice and Bob's interactions
 await Promise.all([
   ctcAlice.p.Alice({
-    // implement Alice's interact object here
+    ...Player('Alice'),
+    wager: stdlib.parseCurrency(5),
   }),
   ctcBob.p.Bob({
-    // implement Bob's interact object here
+    ...Player('Bob'),
+    acceptWager: (amt) => {
+        console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+    },
   }),
 ]);
+
+const afterAlice = await getBalance(accAlice);
+const afterBob = await getBalance(accBob);
+
+console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
+console.log(`Bob went from ${beforeBob} to ${afterBob}.`);
---
+++ index.rsh
@@ -0,0 +1,55 @@
+'reach 0.1';
+
+const Player = {
+    getHand: Fun([], UInt),
+    seeOutcome: Fun([UInt], Null),
+  };
+  
+export const main = Reach.App(() => {
+
+  //specify the two participants to this application, in our case, Alice and Bob 
+  const Alice = Participant('Alice', {
+    ...Player,
+    wager: UInt,
+  });
+  const Bob   = Participant('Bob', {
+    ...Player,
+    acceptWager: Fun([UInt], Null),
+  });
+  
+  init();
+  
+  //publish alice hand
+  Alice.only(() => {
+    const wager = declassify(interact.wager);
+    const handAlice = declassify(interact.getHand());
+  });
+
+  Alice.publish(wager, handAlice)
+    .pay(wager);
+  commit();
+
+  //publish Bob hand
+  Bob.only(() => {
+    interact.acceptWager(wager);
+    const handBob = declassify(interact.getHand());
+  });
+  Bob.publish(handBob)
+    .pay(wager);
+
+  //compute outcome
+  const outcome = (handAlice + (4 - handBob)) % 3;
+  const            [forAlice, forBob] =
+    outcome == 2 ? [       1,      0] : // <- Oops. was: [2, 0]
+    outcome == 0 ? [       0,      2] :
+    /* tie      */ [       1,      1];
+  transfer(forAlice * wager).to(Alice);
+  transfer(forBob * wager).to(Bob);
+  commit();
+
+  each([Alice, Bob], () => {
+    interact.seeOutcome(outcome);
+  });
+
+
+});
